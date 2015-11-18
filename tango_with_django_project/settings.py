f380bfd2f1e5b8ef514f05c2c8c54e1cb9e7cfa9
daya0576 <daya0576@gmail.com>
2015-11-19 09:07:46 +1100

    django registration redux    登录界面不显示？

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'rango',
+    'registration', # add in the registration package
 )
 
 MIDDLEWARE_CLASSES = (
@@ -112,8 +114,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
 # Auth
-LOGIN_URL = '/rango/login/'
+# LOGIN_URL = '/rango/login/'
 
 # Session
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_COOKIE_AGE = 60*60*24*30
+
+
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+
